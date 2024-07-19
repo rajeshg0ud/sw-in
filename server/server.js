1dac8ac674c6ee7ed5e51aa230a214fda4ed704a
Rajesh <budidirajeshgoud231@gmail.com>
2024-07-19 22:41:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app= express();
 const port= 5000;
  
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://swiggy-india.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers'],
