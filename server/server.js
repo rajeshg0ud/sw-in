9e433a8006f806d3b4abc438864761acb0be3d7b
unknown <budidirajeshgoud231@gmail.com>
2024-07-21 01:54:01 +0530

    q

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import restaurantRouter from "./routes/restaurantsRouter.js";
 import cors from 'cors';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app= express();
 
 const port= 5000;
  
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 app.use(cors({
-    origin: 'https://swiggy-india.vercel.app',
+    origin: NODE_ENV ==='development' ? 'http://localhost:3000' :'https://swiggy-india.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers'],
